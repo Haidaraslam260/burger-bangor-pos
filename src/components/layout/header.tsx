8a8486d551cd0fab33bef157da4faaba1549ba90
Haidaraslam260 <aslamhaidar98@gmail.com>
2026-06-27 21:15:42 +0700

    fix: change client-side signOut parameter from callbackUrl to redirectTo for NextAuth v5

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -101,7 +101,7 @@ export function Header({ user }: HeaderProps) {
                         <DropdownMenuSeparator />
                         <DropdownMenuItem
                             className="text-red-600 focus:text-red-600"
-                            onClick={() => signOut({ callbackUrl: "/login" })}
+                            onClick={() => signOut({ redirectTo: "/login" })}
                         >
                             <LogOut className="mr-2 h-4 w-4" />
                             <span>Log out</span>
